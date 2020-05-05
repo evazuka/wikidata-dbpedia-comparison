0e7359e84a5a2381ef9301ed41162c0e10f6ba3b
Eva <eva.zhuk@gmail.com>
2020-05-05 23:10:42 +0300

    Updates

---
+++ precache-manifest.5f474e923dadb7b41c17bc9b2d2d2f6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a7af89119ea0789d1da74a2cbaafde",
+    "url": "/wikidata-dbpedia-comparison/index.html"
+  },
+  {
+    "revision": "ec6ba0c651c02e7056d0",
+    "url": "/wikidata-dbpedia-comparison/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "498e81c5d6f025a10880",
+    "url": "/wikidata-dbpedia-comparison/static/js/2.fb402a4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wikidata-dbpedia-comparison/static/js/2.fb402a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6ba0c651c02e7056d0",
+    "url": "/wikidata-dbpedia-comparison/static/js/main.4f172cd5.chunk.js"
+  },
+  {
+    "revision": "b6d90a905ee5e5bcaf22",
+    "url": "/wikidata-dbpedia-comparison/static/js/runtime-main.3043db43.js"
+  }
+]);
