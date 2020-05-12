36abe784dc4158b665723f46a098196ef23e42b7
Dmitrijs Minajevs <eva.zhuk@gmail.com>
2020-05-12 19:33:54 +0300

    Basic were added

--- src/TreeComparison.tsx
+++ src/TreeComparison.tsx
@@ -1,6 +1,9 @@
 import React from "react"
 import { RouteComponentProps } from "react-router-dom"
+import 'react-tabs/style/react-tabs.css'
+import './treeComparison.css'
 import WikidataTree from "./WikidataTree"
+import { Tabs, TabList, Tab, TabPanel } from "react-tabs"
 
 class TreeComparison extends React.Component<RouteComponentProps<any>> {
     constructor(props) {
@@ -8,7 +11,23 @@ class TreeComparison extends React.Component<RouteComponentProps<any>> {
     }
     render() {
         return (
-            <WikidataTree wikidataId={this.props.match.params.wikidataId} />
+            <Tabs>
+                <TabList>
+                    <Tab>Statistics</Tab>
+                    <Tab>Wikidata Tree</Tab>
+                    <Tab>DBPedia Tree</Tab>
+                </TabList>
+
+                <TabPanel>
+                    Statistics
+                </TabPanel>
+                <TabPanel>
+                    <WikidataTree wikidataId={this.props.match.params.wikidataId} />
+                </TabPanel>
+                <TabPanel>
+                    DBPedia Tree
+                </TabPanel>
+            </Tabs>
         )
     }
 }
