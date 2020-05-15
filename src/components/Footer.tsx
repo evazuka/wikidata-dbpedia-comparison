6abe1a48470e639085fb714aa358d002ac68676e
Dmitrijs Minajevs <eva.zhuk@gmail.com>
2020-05-15 17:10:48 +0300

    Fixed footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,10 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="footer">
-                All the data used to create family trees and filmography
+                <p>
+                    All the data used to create family trees and filmography
                 is retrieved from <a href="https://www.wikidata.org/">Wikidata</a> and <a href="https://wiki.dbpedia.org/">DBpedia</a>.
+                </p>
             </div>
         )
     }
